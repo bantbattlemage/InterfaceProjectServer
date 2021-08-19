dbac8f131ad386f8a8ee917cb4ab46493fdce2c2
Andrew Hoffman <andrewglennhoffman@gmail.com>
2021-08-18 20:02:29 -0400

    added pulse controller

---
+++ GameServer/Controllers/PulseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GameServer.Controllers
+{
+	[Route("api/[controller]")]
+	public class PulseController : Controller
+	{
+		[HttpGet]
+		public JsonResult Get()
+		{
+			return Json(true);
+		}
+	}
+}
